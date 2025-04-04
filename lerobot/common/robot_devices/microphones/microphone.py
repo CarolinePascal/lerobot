d399ac83836d9f54bc0c5393f572a2952ae8f1ea
CarolinePascal <caroline8.pascal@gmail.com>
2025-04-04 18:24:04 +0200

    Remove variable audio recordings data types (will be converted to float32 anyway)

--- lerobot/common/robot_devices/microphones/microphone.py
+++ lerobot/common/robot_devices/microphones/microphone.py
@@ -116,7 +116,7 @@ class Microphone:
     ```python
     from lerobot.common.robot_devices.microphones.configs import MicrophoneConfig
 
-    config = MicrophoneConfig(microphone_index=0, sampling_rate=16000, channels=[1], data_type="int16")
+    config = MicrophoneConfig(microphone_index=0, sampling_rate=16000, channels=[1])
     microphone = Microphone(config)
 
     microphone.connect()
@@ -136,7 +136,6 @@ class Microphone:
         #Store the recording sampling rate and channels
         self.sampling_rate = config.sampling_rate
         self.channels = config.channels
-        self.data_type = config.data_type
 
         self.mock = config.mock
 
@@ -194,13 +193,6 @@ class Microphone:
         elif self.channels is None:
             self.channels = np.arange(1, actual_microphone["max_input_channels"])
 
-        #TODO(CarolinePascal): Rationalize data types handling according to targeted file format and precision
-        if self.data_type is not None:
-            if self.data_type not in ["int16", "int32", "float32"]:
-                raise OSError(f"Data type {self.data_type} is not supported. Supported data types are ['int8', 'uint8', 'int16', 'int32', 'float32'].")
-        else:
-            self.data_type = "float32"
-
         # Get channels index instead of number for slicing
         self.channels = [c-1 for c in self.channels if c <= actual_microphone["max_input_channels"]]
 
@@ -209,7 +201,7 @@ class Microphone:
             device=self.microphone_index,
             samplerate=self.sampling_rate,
             channels=max(self.channels)+1,
-            dtype=self.data_type,
+            dtype="float32",
             callback=self._audio_callback,
         )
         #Remark : the blocksize parameter could be passed to the stream to ensure that audio_callback always recieve same length buffers.
@@ -226,6 +218,7 @@ class Microphone:
         self.read_queue.put(indata[:,self.channels])
 
     def _record_loop(self, output_file: Path) -> None:
+        #Can only be run on a single process/thread for file writing safety 
         with sf.SoundFile(output_file, mode='x', samplerate=self.sampling_rate,
                       channels=max(self.channels)+1, subtype=sf.default_subtype(output_file.suffix[1:])) as file:
             while not self.record_stop_event.is_set():
